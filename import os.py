b84f5578b9a51970bcc5880ee8441dbf82ee630e
unknown <rezvan.sh05@gmail.com>
2024-04-09 04:58:01 -0700

    update1

--- import os.py
+++ import os.py
@@ -1,18 +1,21 @@
 import os
 
-# تایع برای جستوجو
+# تابع برای جستوجو
 def search(files , string):
     for file in os.listdir(files):
+        i=1
         with open(os.path.join(files , file) , 'r' , encoding="utf-8") as filee:
-            file_txt=filee.read()
-        
-        # جستجو در فایل
-            if string in file_txt:
-                
-                print(f"string *{string}* found in *{file}*:")
-            
+            for str in filee:
+                if string in str:   
+                     print(f"string ***{string}*** found in {file}: line={i}")
+                i+=1
    
- # رشته مد نظر
+
+# رشته مد نظر
 string = input("enter your string: ")
+
+#آدرس فایل ها
 files= 'dickens'
+
+# فراخوانی تابع
 search(files , string)
