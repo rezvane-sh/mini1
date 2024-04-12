78baa72e216c75aebc0645b86f9809eb302ffa49
unknown <rezvan.sh05@gmail.com>
2024-04-12 02:12:06 -0700

    update4

--- import os.py
+++ import os.py
@@ -1,21 +1,20 @@
 import os
-
 # تابع برای جستوجو
 def search(files , string):
     for file in os.listdir(files): #دسترسی به فایل ها
         i=1 #  شمارشگر خط
         with open(os.path.join(files , file) , 'r' , encoding="utf-8") as filee: # باز کردن فایل 
             for str in filee: # جستجوی خط به خط
-                if string in str:   
-                     print(f"string ***{string}*** found in {file}: line={i}")
+                if string in (str:=str.title()):  
+                     print(f"string *{str}*: found in {file}: line={i}")
                 i+=1
+    print ("end")            
    
 
-# رشته مد نظر
-string = input("enter your string: ")
-
-#آدرس فایل ها
-files= 'dickens'
-
-# فراخوانی تابع
-search(files , string)
+while True:
+    # رشته مد نظر
+    string =input("enter your string: ")
+    #آدرس فایل ها
+    files= 'dickens'
+    #فراخوانی تابع
+    search(files , (string:=string.title()))
